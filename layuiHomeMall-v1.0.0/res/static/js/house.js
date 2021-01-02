913841904c75c9b4020672b1635dc47c4bce8313
JumpLin <962394321@qq.com>
2021-01-03 00:56:32 +0800

    拆分

--- layuiHomeMall-v1.0.0/res/static/js/house.js
+++ layuiHomeMall-v1.0.0/res/static/js/house.js
@@ -321,10 +321,9 @@ layui.define(['element', 'carousel', 'table', 'util'], function(exports) {
 				type: 2,
 				title: '发起寄拍',
 				content: 'addSender.html',
-				area: ['730px', '420px'],
+				area: ['730px', '600px'],
 				shade: 0.8,
 				skin: 'address-class',
-				btn: '确定',
 				yes: function(index, layero) {
 					window['layui-layer-iframe' + index].layui.form.on('submit(useradd-submit)', function(data) {
 						layer.close(index);
@@ -342,8 +341,8 @@ layui.define(['element', 'carousel', 'table', 'util'], function(exports) {
 		layer.open({
 			type: 2,
 			title: '详情',
-			content: 'addSender.html',
-			area: ['730px', '420px'],
+			content: 'common-detail.html',
+			area: ['1300px', '1000px'],
 			shade: 0.8,
 			skin: 'address-class',
 			btn: '确定',
